c67fa58286a808742731286c5df040ee3337494b
Alex-GF <yalejandro9@gmail.com>
2026-01-22 16:44:41 +0100

    feat: unit tests for route matching function

--- api/src/test/unit-tests/routeMatcher.test.ts
+++ api/src/test/unit-tests/routeMatcher.test.ts
@@ -5,7 +5,7 @@
  */
 
 import { describe, it, expect } from 'vitest';
-import { matchPath, extractApiPath, findMatchingPattern } from '../main/utils/routeMatcher';
+import { matchPath, extractApiPath, findMatchingPattern } from '../../main/utils/routeMatcher';
 
 describe('routeMatcher', () => {
   describe('matchPath', () => {
